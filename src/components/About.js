2d821de9ea0fcd1d231282f524fa48042d41c647
ravindukariyapperuma <ravindu.kariyapperuma@gmail.com>
2020-08-15 00:02:07 +0530

    Update code

--- src/components/About.js
+++ src/components/About.js
@@ -1,18 +1,15 @@
-import React, { Component } from "react";
-export default class About extends Component {
-  render() {
-    return (
-      <div class="row">
-        <div class="col-md-5">
-          <img src="./logo.png" width="25%"></img>
-        </div>
-        <div class="col-md-5">
-          <h1>IT18121766</h1>
-          <p>
-            Kariyapperuma.K.A.D.R.L<br></br>Y3S1.09.2 (SE)
-          </p>
-        </div>
-      </div>
-    );
-  }
+import React, {Component} from 'react';
+export default class About extends Component{
+    render() {
+        return(
+            <div class="row">
+                <div class="col-md-5">
+                <img src="./logo.png" width="25%" />
+                </div>
+                <div class="col-md-5">
+            <h1>IT18121766</h1>
+            <p>Kariyapperuma.K.A.D.R.L<br></br>Y3S1.09.2 (SE)</p>
+            </div></div>
+        )
+    }
 }
--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,21 +1,17 @@
-import React, { Component } from "react";
-export default class ToDo extends Component {
-  render() {
-    return (
-      <div class="row">
-        <div class="col-md-5">
-          <img src="./logo.png" width="25%"></img>
-        </div>
-        <div class="col-md-5">
-          <h1>
-            <font color="#0F0B2E">TODO APP</font>
-          </h1>
-          <p>
-            This application can add item to list and also can edit delete item
-            from list. Items can add complite items table.
-          </p>
-        </div>
-      </div>
-    );
-  }
+import React, {Component} from 'react';
+export default class ToDo extends Component{
+    render() {
+        return(
+            <div class="row">
+                <div class="col-md-5">
+                <img src="./logo.png" width="25%" />
+                </div>
+                <div class="col-md-5">
+            <h1><font color="#0F0B2E">TODO APP</font></h1>
+            <p>This application can add item to list and also can edit delete item from list. 
+                Items can add complite items table.
+            </p>
+            </div></div>
+        )
+    }
 }
